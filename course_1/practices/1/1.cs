ad608a73f2d802773a267c48627e7ea24806be06
karameltoffee <r.rmarsu@gmail.com>
2025-11-27 18:53:47 +0300

    feat: practice 1

--- course_1/practices/1/1.cs
+++ course_1/practices/1/1.cs
@@ -8,7 +8,7 @@ class Program
         int a = 12;
         int b = 5;
 
-        Console.WriteLine($"Сумма: {a+b}");
+        Console.WriteLine($"Сумма: {a + b}");
         Console.WriteLine($"Разность: {a - b}");
         Console.WriteLine($"Произведение: {a * b}");
         Console.WriteLine($"Частное:  {a / b} ");
@@ -26,5 +26,39 @@ class Program
         int num = int.Parse(Console.ReadLine());
         Console.WriteLine(num * 19);
 
+
+        // 4
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        Console.WriteLine(width* height);
+
+        
+        // 5
+        int cel = int.Parse(Console.ReadLine());
+        Console.WriteLine(cel*9/5+32);
+
+
+        // 6
+        int d = int.Parse(Console.ReadLine());
+        int e = int.Parse(Console.ReadLine());
+        int f = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Среднее арифметическое: {(d+e+f)/3}");
+
+        // 7
+        int g = int.Parse(Console.ReadLine());
+        int h = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Сумма: {g + h}");
+        Console.WriteLine($"Разность: {g - h}");
+        Console.WriteLine($"Произведение: {g * h}");
+        Console.WriteLine($"Частное:  {g / h} ");
+
+
+        // 8
+        int rub = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Dollar : {78.45 * rub}");
+        Console.WriteLine($"Euro : {90.23 * rub}");
+        Console.WriteLine($"Yuan : {11.03 * rub}");
     }
 }
+
